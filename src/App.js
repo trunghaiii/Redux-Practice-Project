c314e98d4e723714fede525a7c980d543cef7449
trunghaiii <haitran022001@gmail.com>
2023-04-04 22:31:03 -0400

    config axios and test api with axios

--- src/App.js
+++ src/App.js
@@ -7,36 +7,63 @@ import {
   increaseCounter,
   decreaseCounter,
 } from "./action/actions"
+import axios from 'axios'
 
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
 function App(props) {
+
+  const dispatch = useDispatch();
+  const newCount = useSelector((state) => { return state.counter.count });
+
+  // event handler
+  const handleIncrease = () => {
+
+    // dispatch
+    dispatch(increaseCounter())
+  }
+
+  const fetchAllUser = async () => {
+    const res = await axios.get("http://localhost:8080/users/all")
+    const data = res && res.data ? res.data : []
+    console.log(res);
+  }
+
+  useEffect(() => {
+    fetchAllUser();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and Hoi Dan IT!</h1>
 
-        <div>Count: {props.count}</div>
+        <div>Count: {newCount}</div>
 
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
+        <button onClick={() => handleIncrease()}>Increase Count</button>
 
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
       </header>
     </div>
   );
 }
 
-const mapStateToProps = state => {
-  return {
-    count: state.counter.count,
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     count: state.counter.count,
+//   }
+// }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  }
-}
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+//export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+export default App
+
