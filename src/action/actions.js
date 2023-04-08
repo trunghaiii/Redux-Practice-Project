c7eff88a7f8120e94fc32b3c540599fd67ad0d69
trunghaiii <haitran022001@gmail.com>
2023-04-07 23:19:27 -0400

    create new user with redux

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,6 +1,7 @@
 import {
     INCREMENT, DECREMENT,
-    FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_ERROR
+    FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_ERROR,
+    CREATE_USER_REQUEST, CREATE_USER_SUCCESS, CREATE_USER_ERROR
 } from './types';
 import axios from 'axios'
 
@@ -59,5 +60,44 @@ export const fetchUsersSuccess = (data) => {
 export const fetchUsersError = () => {
     return {
         type: FETCH_USER_ERROR
+    }
+}
+
+
+export const createUsersRequest = () => {
+    return {
+        type: CREATE_USER_REQUEST
+    }
+}
+
+export const createUsersSuccess = () => {
+    return {
+        type: CREATE_USER_SUCCESS
+    }
+}
+
+export const createUsersError = () => {
+    return {
+        type: CREATE_USER_ERROR
+    }
+}
+
+export const createNewUserRedux = (email, password, username) => {
+    return async (dispatch, getState) => {
+
+        dispatch(createUsersRequest())
+
+        try {
+            let res = await axios.post("http://localhost:8080/users/create", { email, password, username })
+
+            if (res && res.data.errCode === 0) {
+                dispatch(createUsersSuccess())
+                dispatch(fetchAllUser())
+            }
+
+        } catch (error) {
+            console.log(error);
+            dispatch(createUsersError())
+        }
     }
 }
--- src/components/FormAddNew.js
+++ src/components/FormAddNew.js
@@ -1,25 +1,56 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/esm/Button';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { createNewUserRedux } from "../action/actions"
 const FormAddNew = (props) => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [username, setUsername] = useState("")
+    const isCreating = useSelector(state => state.user.isCreating)
+
+    const dispatch = useDispatch()
+
+    const handleCreateNewUser = () => {
+        dispatch(createNewUserRedux(email, password, username))
+        setEmail("")
+        setPassword("")
+        setUsername("")
+    }
     return (
         <>
             <Container>
                 <Form>
                     <Form.Group className="mb-3">
                         <Form.Label>Email address: </Form.Label>
-                        <Form.Control type="email" />
+                        <Form.Control
+                            type="email"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Password: </Form.Label>
-                        <Form.Control type="password" />
+                        <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)}
+                        />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Username: </Form.Label>
-                        <Form.Control type="text" />
+                        <Form.Control
+                            type="text"
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)}
+                        />
                     </Form.Group>
-                    <Button>Create</Button>
+                    <Button
+                        onClick={() => handleCreateNewUser()}
+                        disabled={isCreating}
+                    >Create</Button>
 
                 </Form>
 
